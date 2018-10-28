4cc186c3e3b586fa1c5ba45b472f1f07cedbe150
Zach Izzard <izzard@usc.edu>
2018-10-28 14:00:01 -0700

    Added saved items div

--- perigo/scripts.js
+++ perigo/scripts.js
@@ -1,5 +1,6 @@
 function clickedHeart(element){
-	console.log(element.getAttribute("data-itemID"));
+	var elementID = element.getAttribute("data-itemID");
+	console.log(elementID);
 
 	if(element.innerHTML === "<i class=\"fas fa-heart\"></i>"){
 		element.innerHTML = "<i class='far fa-heart'></i>";
@@ -9,4 +10,23 @@ function clickedHeart(element){
 		element.innerHTML = "<i class='fas fa-heart'></i>";
 		element.style.color = "#FF4755";
 	}
+};
+
+function getItem(element){
+	var elementID = element.getAttribute("data-itemID");
+	console.log(elementID);
+};
+
+function unsaveItem(element){
+	var elementID = element.getAttribute("data-itemID");
+	console.log(elementID);
+};
+
+function showSavedItems(){
+    var table = document.getElementById("saved-items");
+    if (table.style.display === "none") {
+        table.style.display = "block";
+    } else {
+        table.style.display = "none";
+    }
 };
